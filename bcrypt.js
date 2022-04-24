39475983846db46aeaddf57293ce5c8875018121
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-04-24 16:29:00 +0600

    Create bcrypt.js

---
+++ bcrypt.js
@@ -0,0 +1,11 @@
+// Encrypts password to SHA-256 HASHS including salt rounds. // 
+// modules playground, use to test modules
+const bcrypt = require('bcrypt');
+const test = "A5-S071";
+
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';     
+const salt = bcrypt.genSaltSync(saltRounds);
+const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+
+console.log(hash);
