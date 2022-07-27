081a07b76eac7b31100127f88dc32f4d84ec6f03
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-07-28 03:51:57 +0600

    Update Alph.java

--- Alph.java
+++ Alph.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Alph {
     public static void main (String [] args){
-
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
         if (s.length()%2==0){
@@ -11,3 +10,5 @@ public class Alph {
         }
     }
 }
+
+
