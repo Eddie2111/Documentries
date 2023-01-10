c1763125e9e37a544db9560c41a41266e7e2d676
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-01-10 17:38:31 +0600

    go.mod and go api should be in the same folder

---
+++ GoRestAPI.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	
+)
+
+func main() {
+	m := map[string]int{
+		"apple":  1,
+		"banana": 2,
+		"orange": 3,
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			fmt.Fprint(w, m)
+		} else if r.Method == http.MethodPost {
+			fmt.Fprint(w, m)
+		}
+	})
+
+	http.ListenAndServe(":3200", nil)
+}
